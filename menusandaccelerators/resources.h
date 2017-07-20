58b4c44982ddeabceee6a134bde27d1a440e088a
Jackson Siro <smataweb@gmail.com>
2017-07-20 09:59:38 +0300

    Create resources.h

---
+++ menusandaccelerators/resources.h
@@ -0,0 +1,13 @@
+#define IDD_DIALOG1                200
+
+#define IDR_MENU1                  10000
+#define IDM_OPEN                   10001
+#define IDM_SAVE                   10002
+#define IDM_COPY                   10003
+#define IDM_CUT                    10004
+#define IDM_PASTE                  10005
+#define IDM_UNDO                   10006
+#define IDM_HELP                   10007
+#define IDM_ABOUT                  10008
+
+#define IDR_ACCELERATOR1           20000
